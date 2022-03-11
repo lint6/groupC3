c70cff2f6dd25e063821cb15a76436c777f23c37
lint6 <94645121+lint6@users.noreply.github.com>
2022-03-11 09:58:39 +0100

    Update event_data_loss.py

--- event_data_loss.py
+++ event_data_loss.py
@@ -2,11 +2,24 @@ import data_importer
 import numpy as np
 import scipy.io as sio
 
+def fun_hole_checker(array_in, length):
+    # Function to check for holes in data, this assumes missing data are either zero or missing
+    # 'array_in' should be an narray of the data
+    # 'length' should be the expacted length of the data
+
+    hole_array = np.where(array_in != 0, 0, 1)
+    if len(hole_array) != length:
+        raise ValueError('Length of the array is shorter than expacted, missing data detected')
+    hole_array = np.nonzero(hole_array) # find location of all zero value within array_in
+    
+    return hole_array
+
+
+
 index = data_importer.fun_Index_Gen("Data files")
 index_num = int(input('Which file number would you like?'))
-
-#Extracting Data from Matlab
 mat = sio.loadmat(f'Data files/{index[index_num]}')
 
 array = data_importer.fun_data_format(mat)
-print(array)
+
+print(fun_hole_checker(array, len(array)))
