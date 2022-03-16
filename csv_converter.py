7decc040facc28f93ec07d194067bb7d99a3b152
lint6 <94645121+lint6@users.noreply.github.com>
2022-03-16 11:07:14 +0100

    converted data files to .csv

--- csv_converter.py
+++ csv_converter.py
@@ -2,15 +2,24 @@ import data_importer
 import numpy as np
 import scipy.io as sio
 
-def data_rate_checker(array_in):
-    time_array = array_in[0]
-    time_array_2 = np.pad(time_array, (1,0))
-    time_array = np.pad(time_array, (0,1))
-    time_diff = np.delete(time_array - time_array_2, [0, -1] , 0)
-    delta = np.bincount(time_diff.astype("int64")).argmax()
-    error_array = np.nonzero(time_diff == delta)
-    if error_array[0].size > 0:
-        print('Data rate error found, location printed:')
-        print(error_array)
-    else:
-        print('No data rate error found')
+def rewriter(array_in, index_num):
+    global index
+    #print(array_in)
+    #print(np.shape(array_in))
+    with open (f'csv_data/{index[index_num]}.csv',"w") as f:
+        f.write("")
+    with open (f'csv_data/{index[index_num]}.csv', 'a') as f:
+        for i in range(len(array_in[0])):
+            f.write(f'{array_in[6][i]},{array_in[7][i]},{array_in[8][i]},{array_in[3][i]},{array_in[4][i]},{array_in[5][i]}')
+            #x, y, z, roll, pitch, yaw
+            f.write("\n")
+    print('done')
+
+index = data_importer.fun_Index_Gen("Data files")
+#index_num = int(input('Which file number would you like?'))
+for i in range(26):
+    print(f'\nData file{i}')
+    index_num = i
+    array = sio.loadmat(f'Data files/{index[index_num]}')
+    array = data_importer.fun_data_format(array)
+    rewriter(array, index_num)
