914090fe1273c968d28da943c039db9bf375fa3b
lint6 <94645121+lint6@users.noreply.github.com>
2022-06-01 09:21:09 +0200

    Update ROC-rng-based.py

--- ROC-rng-based.py
+++ ROC-rng-based.py
@@ -3,7 +3,7 @@ import numpy as np
 from numpy import random
 # import cupy as cp
 import matplotlib.pyplot as plt
-n = 19
+n = 50
 x_plot1 = [0,1]
 y_plot1 = [0,1]
 # x_plot2 = np.arange(0,1,0.01)
@@ -20,7 +20,7 @@ x_plot2 = x_plot2 +  0.03*(random.rand(1)-0.5)
 # print(y_plot2)
 print(x_rand)
 plt.plot(x_plot1, y_plot1)
-plt.scatter(x_plot2, y_plot2, s=1)
+plt.scatter(x_plot2, y_plot2, s=2)
 plt.xlim(0, 1)
 plt.ylim(0, 1)
 plt.show()
